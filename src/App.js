12802ca47e42e465c2bc0cf2a0b380e8bfdc6e51
Nabeel <nabeel.siddiqui@rocketmail.com>
2021-03-19 10:44:44 +0400

    new photos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Carousel from './Carousel';
+import Card from './Card';
 import NavBar from './NavBar';
+
+
+const cardContent = [
+  {
+    title: 'Visit Dubai',
+    description: 'Dubai is the commercial center of UAE',
+    imgSrc: './abudhabi1'
+  },
+  {
+    title: 'Explore Abu Dhabi',
+    description: 'Click to learn about the beautiful capital of the UAE',
+    imgSrc: './zayed-mosque.jpg'
+  },
+  {
+    title: 'Ras Al Khaimah Tour',
+    description: 'The natural resort of the UAE. Click here to learn more!',
+    imgSrc: 'https://images.unsplash.com/photo-1591609282229-c080f8c25ef4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80'
+  },
+]
+
+
+{/*const CounterButton = () => {
+  
+  const [state, setState] = useState(0);
+  
+    
+
+return (<button className="btn-light">{state}</button>) */}
+
 
 
 function App() {
   return (
     <div>
+
+      { /* Main navgation */ }
       <NavBar 
         brand="My App" 
         links={
@@ -27,9 +59,37 @@ function App() {
       />
 
       <div className="content" style={{minHeight: 'calc(100vh - 112px)'}}>
+       
+       { /* Carousel goes here */ }
        <Carousel images={['./zayed-mosque.jpg', './dubai-night.jpg']}/>
+
+       { /* Cards go here */ }
+       <div className="container my-4">
+         <div className="row">
+
+            {
+              cardContent.map(
+                (arrElement) => {
+                    return (
+                      <div className="col-12 col-md-6 col-lg-4">
+                        <Card 
+                            className="mb-4 m-auto"
+                            title={arrElement.title}
+                            imgSrc={arrElement.imgSrc}
+                            description={arrElement.description}
+                        />
+                      </div>
+                    )
+                }
+              )
+            }
+            
+          </div>
+       </div>
+
       </div>
 
+      { /* Footer navigation */ }
       <NavBar 
         brand="My App" 
         links={
--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = (props) => {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">{props.brand}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
@@ -13,13 +13,21 @@ const NavBar = (props) => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">{props.links[0]}</a>
+                <a className="nav-link active" 
+                aria-current="page" 
+                href={props.links[0].path}>
+                    {props.links[0].label}
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">{props.links[1]}</a>
+                <a className="nav-link" href={props.links[1].path}>
+                    {props.links[1].label}
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">{props.links[2]}</a>
+                <a className="nav-link" href={props.links[2].path}>
+                    {props.links[2].label}
+                </a>
               </li>
             </ul>
           </div>
